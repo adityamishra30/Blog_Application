fc112a4820a164571ed1a5d6628ba5256575e6a2
Aditya Mishra <adityame222@gmail.com>
2026-07-24 02:24:26 +0530

    initial commit

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# pyrefly: ignore [missing-import]
+from . import views
+urlpatterns = [
+    path("",views.login_view,name="login"),
+    path("register/",views.register_view,name="register"),
+    path("home/",views.home,name="home"),
+
+]
